23960d3d07f4a5226987bc4a3b2a3f4ad00b46fc
KIM MIN WOO <79193811+minwoo1999@users.noreply.github.com>
2024-01-05 13:05:50 +0900

    :wrench: foodlog 초기셋팅

---
+++ food-log/src/test/java/com/example/foodlog/FoodLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
